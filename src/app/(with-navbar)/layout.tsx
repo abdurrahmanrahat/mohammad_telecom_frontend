cabe0843d008b6ceda8ad812263ca184d3423e17
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-05-31 11:21:13 +0600

    whatsapp integration

--- src/app/(with-navbar)/layout.tsx
+++ src/app/(with-navbar)/layout.tsx
@@ -1,14 +1,16 @@
 import Footer from "@/components/shared/Ui/Footer/Footer";
 import Navbar from "@/components/shared/Ui/Navbar/Navbar";
+import WhatsAppLive from "@/components/shared/Ui/WhatsAppLive";
 import { ReactNode } from "react";
 
 const CommonLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
+    <div className="relative">
       <Navbar />
       <div className="min-h-screen">{children}</div>
       <Footer />
-    </>
+      <WhatsAppLive />
+    </div>
   );
 };
 
--- src/image-data/shared.image.ts
+++ src/image-data/shared.image.ts
@@ -1,3 +1,4 @@
 import GoogleLogo from "../../public/images/shared/google_logo.png";
+import WhatsAppLogo from "../../public/images/shared/whatsapp2.png";
 
-export const SharedImages = { GoogleLogo };
+export const SharedImages = { GoogleLogo, WhatsAppLogo };
